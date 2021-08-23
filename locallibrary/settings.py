14fa29be9fa55cb4e2f4998ed4c54a54a9d1efb3
Kavian <kavianam@gmail.com>
2021-08-23 21:36:30 +0430

    add STATIC_ROOT to the settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -133,6 +133,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
